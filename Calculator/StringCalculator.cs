fcdcc8ad604c5b20236d95f11bd4520db4b7b989
Andres Escobar <afescobarf@gmail.com>
2012-05-24 08:24:08 -0500

    prueba en csharp

---
+++ Calculator/StringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class StringCalculator
+    {
+        public static int add(string number)
+        {
+            if (number == "") return 0;
+            int ans = 0;
+            String[] array;
+            char[] r=new char[3];
+            if (number.Length > 2 && number.StartsWith("//"))
+            {
+                r[0] = number[2];
+                r[1] = '\n';
+                r[2] = '/';
+            }
+            else
+            {
+                r[0] = ',';
+                r[1] = '\n';
+            }
+            array = number.Split(r, StringSplitOptions.RemoveEmptyEntries);
+            string mess="negatives not allowed: ";
+            for (int i = 0; i < array.Length; i++)if(int.Parse(array[i])<0)
+            {
+                mess += int.Parse(array[i]) + " ";
+            }
+            if (mess != "negatives not allowed: ")
+            {
+                throw new Exception(mess);
+                //throw new System.ArgumentOutOfRangeException(mess);
+            }
+            for (int i = 0; i < array.Length; i++) if (int.Parse(array[i]) <= 1000)
+            {
+                ans += int.Parse(array[i]);
+            }
+            return ans;
+        }
+    }
+}
---
+++ NUnitTest/StringCalculatorTest.cs
@@ -0,0 +1,101 @@
+﻿using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NUnitTest
+{
+    [TestClass()]
+    public class StringCalculatorTest
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod()]
+        public void empty_string()
+        {
+            string cad = string.Empty;
+            int expected = 0; 
+            int actual = StringCalculator.add(cad);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void one_number()
+        {
+            string cad = "999";
+            int expected = 999;
+            int actual = StringCalculator.add(cad);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void two_numbers()
+        {
+            string cad = "74,799";
+            int expected = 873;
+            int actual = StringCalculator.add(cad);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void un_known_amount_of_numbers()//Suma los numeros del 1 al 100 (el resultado es 5050)
+        {
+            string cad = "";
+            int expected = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                cad += i + ",";
+                expected += i;
+            }
+            int actual = StringCalculator.add(cad);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void new_lines_between_numbers()
+        {
+            string cad = "15\n30,120";
+            int expected = 165;
+            int actual = StringCalculator.add(cad);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void support_different_delimiters()
+        {
+            string cad = "//;\n100;200";
+            int expected = 300;
+            int actual = StringCalculator.add(cad);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void negatives_not_allowed ()
+        {
+            String cad = "78,8,9,-20,-41,-899,89";
+            String expected = "negatives not allowed: -20 -41 -899 ";
+            try
+            {
+                StringCalculator.add(cad);
+            }
+            catch(Exception ex)
+            {
+                Assert.AreEqual(expected,ex.Message);
+            }
+
+        }   
+        [TestMethod()]
+        public void no_more_than_1000()
+        {
+            string cad = "//;\n100;500;600;1200;2400";
+            int expected = 1200;
+            int actual = StringCalculator.add(cad);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
